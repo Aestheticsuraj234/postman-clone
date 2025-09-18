a928f6f81ac8df656550d002fd643b9fb1c6e5a8
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-09-18 11:39:37 +0530

    03 Authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import db from "@/lib/db";
+import { env } from "@/lib/env";
+
+
+
+export const auth = betterAuth({
+    database: prismaAdapter(db, {
+        provider: "postgresql", 
+    }),
+
+    socialProviders:{
+        github:{
+            clientId:env.GITHUB_CLIENT_ID,
+            clientSecret:env.GITHUB_CLIENT_SECRET
+        }
+    }
+});
