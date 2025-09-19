31c2038ed8ed0e3ee5ebdd9a26ad0f55b2dba60f
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-09-19 10:24:56 +0530

    05 Workspace

--- src/app/(workspace)/layout.tsx
+++ src/app/(workspace)/layout.tsx
@@ -1,12 +1,16 @@
 import { currentUser } from '@/modules/authentication/actions'
 import Header from '@/modules/Layout/components/header'
+import { initializeWorkspace } from '@/modules/workspace/actions'
 import React from 'react'
 
-const RootLayout = async({children}:{children: React.ReactNode}) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const workspace = await initializeWorkspace()
+  console.log(JSON.stringify(workspace))
   const user = await currentUser()
   return (
     <main className='w-screen h-screen '>
-        <Header user={user}/>
+      {/* @ts-ignore */}
+      <Header user={user!} workspace={workspace.workspace!} />
       {children}
     </main>
   )
